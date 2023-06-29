3fa46402efd435dddac0462003904d8ce5e04d37
Jonas <36644388+QC20@users.noreply.github.com>
2023-06-30 01:23:34 +0200

    Add files via upload

---
+++ Rename-files.py
@@ -0,0 +1,20 @@
+import os
+
+def rename_images(folder_path):
+    file_extension = ".jpg"  # Change to ".jpeg" if necessary
+    counter = 1
+
+    for filename in os.listdir(folder_path):
+        if filename.endswith(file_extension):
+            old_name = os.path.join(folder_path, filename)
+            new_name = os.path.join(folder_path, str(counter) + file_extension)
+
+            os.rename(old_name, new_name)
+            counter += 1
+
+    print("Images renamed successfully!")
+
+# Folrder Path
+folder_path = "/Users/jonaskjeldmand/Desktop/Arduino-board-test-code"
+#folder_path = "path/to/folder"  # Replace with your folder path
+rename_images(folder_path)
